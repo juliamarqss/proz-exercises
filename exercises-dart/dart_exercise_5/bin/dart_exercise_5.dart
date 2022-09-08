f03b6217bb230ca3b022cfd3625adb4a2ca9ff4a
Júlia Marques <juliamarques61@gmail.com>
2022-09-08 15:46:38 -0300

    feat: iniciando exercicio 5

---
+++ exercises-dart/dart_exercise_5/bin/dart_exercise_5.dart
@@ -0,0 +1,5 @@
+import 'package:dart_exercise_5/dart_exercise_5.dart' as dart_exercise_5;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_exercise_5.calculate()}!');
+}
---
+++ exercises-dart/dart_exercise_5/test/dart_exercise_5_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_exercise_5/dart_exercise_5.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
