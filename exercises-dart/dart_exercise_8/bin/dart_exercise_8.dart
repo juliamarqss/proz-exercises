dfe391b5123190c1c1dc9dce42a747d922a2e159
Júlia Marques <juliamarques61@gmail.com>
2022-09-14 16:53:13 -0300

    feat: iniciando o desafio final de dart

---
+++ exercises-dart/dart_exercise_8/bin/dart_exercise_8.dart
@@ -0,0 +1,5 @@
+import 'package:dart_exercise_8/dart_exercise_8.dart' as dart_exercise_8;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_exercise_8.calculate()}!');
+}
---
+++ exercises-dart/dart_exercise_8/test/dart_exercise_8_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_exercise_8/dart_exercise_8.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
